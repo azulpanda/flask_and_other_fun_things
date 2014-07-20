03ae7060760c37140f42569e7e7d0f6a52c2db3d
Dae Hong Kim <grizzlyraccoon@gmail.com>
2014-07-21 08:43:25 +0900

    4

--- flaskr.py
+++ flaskr.py
@@ -30,3 +30,10 @@ def get_db():
 def close_db(error):
 	if hasattr(g, 'sqlite_db'):
 		g.sqlite_db.close()
+
+def init_db():
+	with app.app_context():
+		db = get_db()
+		with app.open_resource('schema.sql', mode='r') as f:
+			db.cursor().executescript(f.read())
+		db.commit()
